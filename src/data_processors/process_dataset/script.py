85faf3772e3034b10fe7d250a1e06b494834643b
Luke Zappia <luke@data-intuitive.com>
2024-11-06 16:14:30 +0100

    Add waypoints and centroids to dataset processing

--- src/data_processors/process_dataset/script.py
+++ src/data_processors/process_dataset/script.py
@@ -2,12 +2,14 @@ import sys
 
 import anndata as ad
 import openproblems as op
+import numpy as np
+from sklearn.metrics import pairwise_distances
 
 ## VIASH START
 par = {
     "input": "resources_test/common/cxg_mouse_pancreas_atlas/dataset.h5ad",
-    "output_dataset": "train.h5ad",
-    "output_solution": "test.h5ad",
+    "output_dataset": "dataset.h5ad",
+    "output_solution": "solution.h5ad",
 }
 meta = {
     "resources_dir": "target/executable/data_processors/process_dataset",
@@ -15,24 +17,65 @@ meta = {
 }
 ## VIASH END
 
-
 # import helper functions
 sys.path.append(meta["resources_dir"])
 from subset_h5ad_by_format import subset_h5ad_by_format
 
+print("====== Dataset processor ======", flush=True)
+
+print("\n>>> Reading processor config...", flush=True)
 config = op.project.read_viash_config(meta["config"])
 
-print(">> Load Data", flush=True)
+print("\n>>> Reading raw dataset...", flush=True)
 adata = ad.read_h5ad(par["input"])
+print(adata, flush=True)
 
-print(adata)
+print("\n>>> Selecting waypoint cells...", flush=True)
+if adata.n_obs < 10000:
+    waypoint_cells = adata.obs_names
+else:
+    np.random.seed(0)  # Try to get the same cells each time
+    waypoint_cells = np.random.choice(adata.obs_names, 10000, replace=False)
+adata.obs["is_waypoint"] = adata.obs_names.isin(waypoint_cells)
 
-print(">> Creating input data", flush=True)
+print("\n>>> Calculating waypoint distances...", flush=True)
+adata.obsm["waypoint_distances"] = pairwise_distances(
+    adata.layers["normalized"],
+    adata.layers["normalized"][adata.obs["is_waypoint"].values, :],
+    metric="euclidean",
+    n_jobs = -2
+)
+
+print("\n>>> Calculating label centroids...", flush=True)
+normalized_mat = adata.layers["normalized"].todense()
+labels = np.unique(adata.obs["cell_type"])
+centroids = np.zeros((len(labels), normalized_mat.shape[1]))
+for i, label in enumerate(labels):
+    is_label = adata.obs["cell_type"] == label
+    centroids[i, :] = np.mean(normalized_mat[is_label, :], axis=0)
+
+adata.uns["label_centroids"] = centroids
+
+print("\n>>> Calculating centroid distances...", flush=True)
+adata.obsm["centroid_distances"] = pairwise_distances(
+    adata.layers["normalized"],
+    centroids,
+    metric="euclidean",
+    n_jobs = -2
+)
+
+print("\n>>> Creating input data...", flush=True)
 output_dataset = subset_h5ad_by_format(adata, config, "output_dataset")
+print(output_dataset, flush=True)
 
-print(">> Creating solution data", flush=True)
+print("\n>>> Creating solution data...", flush=True)
 output_solution = subset_h5ad_by_format(adata, config, "output_solution")
+print(output_solution, flush=True)
 
-print(">> Writing data", flush=True)
+print("\n>>> Writing data files...", flush=True)
 output_dataset.write_h5ad(par["output_dataset"])
+print(f"Output dataset file: '{par['output_dataset']}'", flush=True)
 output_solution.write_h5ad(par["output_solution"])
+print(f"Output solution file: '{par['output_solution']}'", flush=True)
+
+print("\n>>> Done!", flush=True)
