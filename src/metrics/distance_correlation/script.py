bc5ba8c164e46d9e270bfde7b7c8d1f99006bd49
Luke Zappia <luke@data-intuitive.com>
2024-12-05 14:02:52 +0100

    Use pre-computed distances in distance correlation

--- src/metrics/distance_correlation/script.py
+++ src/metrics/distance_correlation/script.py
@@ -1,7 +1,5 @@
 import anndata as ad
-import scipy.spatial
-import scipy.stats
-import sklearn.decomposition
+import scipy
 
 ## VIASH START
 par = {
@@ -11,38 +9,36 @@ par = {
 }
 ## VIASH END
 
+print(f"====== Distance correlation metrics (scipy v{scipy.__version__}) ======", flush=True)
 
-def _distance_correlation(X, X_emb):
-    high_dimensional_distance_vector = scipy.spatial.distance.pdist(X)
-    low_dimensional_distance_vector = scipy.spatial.distance.pdist(X_emb)
-    corr = scipy.stats.spearmanr(
-        low_dimensional_distance_vector, high_dimensional_distance_vector
-    )
-    return corr
+print("\n>>> Reading solution...", flush=True)
+solution = ad.read_h5ad(par["input_solution"])
+print(solution, flush=True)
 
+print("\n>>> Reading embedding...", flush=True)
+embedding = ad.read_h5ad(par["input_embedding"])
+print(embedding, flush=True)
 
-print("Load data", flush=True)
-input_solution = ad.read_h5ad(par["input_solution"])
-input_embedding = ad.read_h5ad(par["input_embedding"])
+print("\n>>> Calculating distance correlation..", flush=True)
+high_dists = solution.obsm["waypoint_distances"]
+emb_dists = embedding.obsm["waypoint_distances"]
+dist_corr = scipy.stats.spearmanr(high_dists, emb_dists, axis=None).correlation
+print(f"Distance correlation: {dist_corr}", flush=True)
 
-high_dim = input_solution.layers["normalized"]
-X_emb = input_embedding.obsm["X_emb"]
-
-print("Compute NNLS residual after SVD", flush=True)
-n_svd = 500
-svd_emb = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(high_dim)
-dist_corr = _distance_correlation(svd_emb, X_emb).correlation
-
-print("Create output AnnData object", flush=True)
+print("\n>>> Creating output AnnData object...", flush=True)
 output = ad.AnnData(
     uns={
-        "dataset_id": input_solution.uns["dataset_id"],
-        "normalization_id": input_solution.uns["normalization_id"],
-        "method_id": input_embedding.uns["method_id"],
+        "dataset_id": solution.uns["dataset_id"],
+        "normalization_id": solution.uns["normalization_id"],
+        "method_id": embedding.uns["method_id"],
         "metric_ids": ["distance_correlation"],
         "metric_values": [dist_corr],
     }
 )
+print(output, flush=True)
 
-print("Write data to file", flush=True)
+print("\n>>> Writing output file...", flush=True)
 output.write_h5ad(par["output"], compression="gzip")
+print(f"Output file: '{par['output']}'", flush=True)
+
+print("\n>>> Done!", flush=True)
